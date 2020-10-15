f607ce9e7d539b732f9af845cd7def1e792c81b1
Dong-Hun Gang <1031nice@gmail.com>
2020-10-15 09:38:17 +0900

    BOARDCOVER 개선

--- src/APSS/BOARDCOVER.java
+++ src/APSS/BOARDCOVER.java
@@ -47,34 +47,31 @@ public class BOARDCOVER {
     }
 
     private static int func(char[][] arr, int index, int count) {
-        if(index >= arr.length * arr[0].length)
+        if(count == 0)
+            return 1;
+        else if(index >= arr.length * arr[0].length)
             return 0;
         else if(check(arr, index))
             return 0;
-        else if(count == 0)
-            return 1;
         else {
             int ret = 0;
-            for(int i=index; i<arr.length * arr[0].length; i++) {
-                if(arr[i / arr[0].length][i % arr[0].length] == '#')
-                    continue;
-                if (pick(arr, i, 1)) { // 1사분면 모양
-                    ret += func(arr, i+1, count - 3);
-                    unpick(arr, i, 1);
-                }
-                if (pick(arr, i, 2)) { // 2사분면 모양
-                    ret += func(arr, i+1, count - 3);
-                    unpick(arr, i, 2);
-                }
-                if (pick(arr, i, 3)) { // 3사분면 모양
-                    ret += func(arr, i+1, count - 3);
-                    unpick(arr, i, 3);
-                }
-                if (pick(arr, i, 4)) { // 4사분면 모양
-                    ret += func(arr, i+1, count - 3);
-                    unpick(arr, i, 4);
-                }
+            if (pick(arr, index, 1)) { // 1사분면 모양
+                ret += func(arr, index+1, count - 3);
+                unpick(arr, index, 1);
             }
+            if (pick(arr, index, 2)) { // 2사분면 모양
+                ret += func(arr, index+1, count - 3);
+                unpick(arr, index, 2);
+            }
+            if (pick(arr, index, 3)) { // 3사분면 모양
+                ret += func(arr, index+1, count - 3);
+                unpick(arr, index, 3);
+            }
+            if (pick(arr, index, 4)) { // 4사분면 모양
+                ret += func(arr, index+1, count - 3);
+                unpick(arr, index, 4);
+            }
+            ret += func(arr, index+1, count);
             return ret;
         }
     }
@@ -125,7 +122,7 @@ public class BOARDCOVER {
         int col = index % arr[0].length;
         switch (dir) {
             case 1:
-                if (row - 1 < 0 || col + 1 >= arr[0].length || arr[row-1][col] == '#' || arr[row][col+1] == '#')
+                if (row - 1 < 0 || col + 1 >= arr[0].length || arr[row][col] == '#' || arr[row-1][col] == '#' || arr[row][col+1] == '#')
                     return false;
                 else {
                     arr[row][col] = '#';
@@ -134,7 +131,7 @@ public class BOARDCOVER {
                     return true;
                 }
             case 2:
-                if (row + 1 >= arr.length || col + 1 >= arr[0].length || arr[row+1][col] == '#' || arr[row][col+1] == '#')
+                if (row + 1 >= arr.length || col + 1 >= arr[0].length || arr[row][col] == '#' || arr[row+1][col] == '#' || arr[row][col+1] == '#')
                     return false;
                 else {
                     arr[row][col] = '#';
@@ -143,7 +140,7 @@ public class BOARDCOVER {
                     return true;
                 }
             case 3:
-                if (row + 1 >= arr.length || col - 1 < 0 || arr[row+1][col] == '#' || arr[row][col-1] == '#')
+                if (row + 1 >= arr.length || col - 1 < 0 || arr[row][col] == '#' || arr[row+1][col] == '#' || arr[row][col-1] == '#')
                     return false;
                 else {
                     arr[row][col] = '#';
@@ -152,7 +149,7 @@ public class BOARDCOVER {
                     return true;
                 }
             case 4:
-                if (row - 1 < 0 || col - 1 < 0 || arr[row-1][col] == '#' || arr[row][col-1] == '#')
+                if (row - 1 < 0 || col - 1 < 0 || arr[row][col] == '#' || arr[row-1][col] == '#' || arr[row][col-1] == '#')
                     return false;
                 else {
                     arr[row][col] = '#';
