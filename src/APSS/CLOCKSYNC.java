55a5ab7f8a4cedb8f194c7917e5ea9adc78dc5c4
Dong-Hun Gang <1031nice@gmail.com>
2021-01-08 16:08:43 +0900

    알고리즘문제해결전략 ID QUADTREE(완전탐색)

---
+++ src/APSS/CLOCKSYNC.java
@@ -0,0 +1,87 @@
+package APSS;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
+
+public class CLOCKSYNC {
+
+    public static int[][] switches = {
+            {1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,1,0,0,0,1,0,1,0,1,0,0,0,0},
+            {0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,1},
+            {1,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,1,1,1,0,1,0,1,0,0,0},
+            {1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1},
+            {0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,1},
+            {0,0,0,0,1,1,0,1,0,0,0,0,0,0,1,1},
+            {0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,1,1,1,0,0,0,1,0,0,0,1,0,0}
+    };
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("./src/APSS/inputs/input_CLOCKSYNC.txt"));
+        Iterator<String> iterator = lines.iterator();
+        int testCases = Integer.parseInt(iterator.next());
+        for(int j=0; j<testCases; j++) {
+            String line = iterator.next();
+            String[] tokens = line.split(" ");
+            int[] clocks = new int[tokens.length];
+            for(int i=0; i<tokens.length; i++){
+                clocks[i] = Integer.parseInt(tokens[i]);
+            }
+
+            System.out.println(func(clocks, 0, 0));
+        }
+    }
+
+    private static int func(int[] clocks, int indexOfSwitches, int numOfPush) {
+        if(indexOfSwitches >= switches.length) {
+            return -1;
+        }
+
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<4; i++){ // 같은 버튼을 최대 3회 누를 수 있다
+            if(checkClocks(clocks))
+                return numOfPush;
+            if(i != 0){
+                push(clocks, indexOfSwitches);
+                numOfPush++;
+                if(checkClocks(clocks))
+                    return numOfPush;
+            }
+//            for(int j=indexOfSwitches+1; j<switches.length; j++){
+//                int ret = func(clocks, j, numOfPush);
+//                if(ret > 0) {
+//                    min = Math.min(min, ret);
+//                }
+//            }
+            int ret = func(clocks, indexOfSwitches+1, numOfPush);
+            if(ret > 0)
+                min = Math.min(min, ret);
+        }
+        return min == Integer.MAX_VALUE ? -1 : min;
+    }
+
+    private static void push(int[] clocks, int indexOfSwitches) {
+        for(int i=0; i<clocks.length; i++){
+            if(switches[indexOfSwitches][i] == 1) {
+                clocks[i] += 3;
+                if(clocks[i] > 12)
+                    clocks[i] %= 12;
+            }
+        }
+
+    }
+
+    private static boolean checkClocks(int[] clocks) {
+        for(int i=0; i<clocks.length; i++){
+            if(clocks[i] != 12)
+                return false;
+        }
+        return true;
+    }
+
+}
---
+++ src/APSS/QUADTREE.java
@@ -0,0 +1,39 @@
+package APSS;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
+
+public class QUADTREE {
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("./src/APSS/inputs/input_QUADTREE.txt"));
+        Iterator<String> iterator = lines.iterator();
+        int testCases = Integer.parseInt(iterator.next());
+
+        for(int i=0; i<testCases; i++){
+            String input = iterator.next();
+            System.out.println(func(input));
+        }
+    }
+
+    private static String func(String input) {
+        String[] ret = {"", "", "", ""};
+        int retIndex = 0;
+        int index = 0;
+        while(index < input.length() && retIndex < 4) {
+            if (input.charAt(index) == 'x') {
+                String part = func(input.substring(index+1));
+                ret[retIndex++] = "x" + part;
+                index += part.length()+1;
+            }
+            else {
+                ret[retIndex++] = String.valueOf(input.charAt(index++));
+            }
+        }
+        return ret[2] + ret[3] + ret[0] + ret[1];
+    }
+
+}
